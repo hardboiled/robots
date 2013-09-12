44ffe2b90bfce40a07789476cb7a076617252356
Daniel Ege <daniel.c.ege@gmail.com>
2013-09-11 23:35:08 -0400

    Moved distance method and now generate random speed

--- src/Robot.java
+++ src/Robot.java
@@ -13,7 +13,7 @@ enum ETrafficConditions {
 
 public class Robot implements Runnable {
     private final double MAX_METERS = 350.0d;
-    private final String ARBITRARY_SPEED = "30 km/hr.";
+    private final int ARBITRARY_SPEED = 80;
     private RoutePoint _curPt;
     private int _id;
     private ArrayList<RoutePoint> _rps;
@@ -41,7 +41,7 @@ public class Robot implements Runnable {
                     for(TunnelPoint tp : trafficPoints) {
                         ETrafficConditions condition = generateRandomTrafficCondition();
                         System.out.println(_id + " " + _curPt.date + ":" + tp.description + " is experiencing " +
-                                            condition + " traffic at speed " + ARBITRARY_SPEED);
+                                            condition + " traffic at speed " + getRandomSpeed(condition));
                     }
                 }
             } else {
@@ -81,11 +81,15 @@ public class Robot implements Runnable {
         int scale = (ETrafficConditions.MaxLength.ordinal() - 1);
         return ETrafficConditions.values()[(int)Math.round(Math.random() * scale)];
     }
+    private String getRandomSpeed(ETrafficConditions type) {
+        int scale = type.ordinal() + 1;
+        return String.format("%.2f km/hr", (ARBITRARY_SPEED * Math.random() / scale));
+    }
     private ArrayList<TunnelPoint> getTrafficPoints() {
         ArrayList<TunnelPoint> tps = new ArrayList<TunnelPoint>();
         synchronized(_tps) {
             for (TunnelPoint tp : _tps) {
-                double meters = distanceInMeters(_curPt.lat, _curPt.lon, tp.lat, tp.lon);
+                double meters = Calculate.distanceInMeters(_curPt.lat, _curPt.lon, tp.lat, tp.lon);
                 if (meters <= MAX_METERS) {
                     tps.add(tp);
                 }
@@ -103,52 +107,4 @@ public class Robot implements Runnable {
         }
         return false;
     }
-    private static double distanceInMeters(double lat1, double lon1, double lat2, double lon2) {
-        //http://stackoverflow.com/questions/120283/working-with-latitude-longitude-values-in-java#answer-9822531
-        double a = 6378137, b = 6356752.314245, f = 1 / 298.257223563; // WGS-84 ellipsoid params
-        double L = Math.toRadians(lon2 - lon1);
-        double U1 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat1)));
-        double U2 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat2)));
-        double sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
-        double sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
-
-        double sinLambda, cosLambda, sinSigma, cosSigma, sigma, sinAlpha, cosSqAlpha, cos2SigmaM;
-        double lambda = L, lambdaP, iterLimit = 100;
-        do {
-            sinLambda = Math.sin(lambda);
-            cosLambda = Math.cos(lambda);
-            sinSigma = Math.sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda)
-                    + (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
-            if (sinSigma == 0)
-                return 0; // co-incident points
-            cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
-            sigma = Math.atan2(sinSigma, cosSigma);
-            sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
-            cosSqAlpha = 1 - sinAlpha * sinAlpha;
-            cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
-            if (Double.isNaN(cos2SigmaM))
-                cos2SigmaM = 0; // equatorial line: cosSqAlpha=0 (§6)
-            double C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
-            lambdaP = lambda;
-            lambda = L + (1 - C) * f * sinAlpha
-                    * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
-        } while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0);
-
-        if (iterLimit == 0)
-            return Double.NaN; // formula failed to converge
-
-        double uSq = cosSqAlpha * (a * a - b * b) / (b * b);
-        double A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
-        double B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
-        double deltaSigma = B
-                * sinSigma
-                * (cos2SigmaM + B
-                / 4
-                * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM
-                * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM)));
-        double dist = b * A * (sigma - deltaSigma);
-
-        return dist;
-    }
-
 }
